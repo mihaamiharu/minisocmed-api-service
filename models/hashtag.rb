32c22f4c706adffb85ec2696fd2d584ee40202e2
mihaamiharu <48645558+mihaamiharu@users.noreply.github.com>
2021-08-21 00:10:24 +0700

    recreate hashtag model and test
    create hooks
    delete necessary variable and method

--- models/hashtag.rb
+++ models/hashtag.rb
@@ -1,44 +1,43 @@
 require_relative '../db/db_connection'
 
-class Hashtag 
+class Hashtag
+  attr_reader :name, :hashtag_id
 
-  attr_reader :hashtag_id, :name
   def initialize(params)
-    @hashtag_id = params[:hashtag_id],
+    @hashtag_id = params[:hashtag_id]
     @name = params[:name]
   end
 
   def valid?
     return false if @name.nil?
+
     true
   end
 
-  def find_hashtag
+  def get_hashtag_id
     client = create_db_client
-    find_hashtag = client.query("SELECT hashtag_id FROM hashtag WHERE hashtag.`name` LIKE '%#{name}%'")
+    get_hashtag_id = client.query("SELECT hashtag_id FROM hashtag WHERE hashtag.`name` LIKE '%#{@name}%'")
 
-    body = find_hashtag
+    data = get_hashtag_id
   end
 
-  def create_hashtag 
+  def post
     return false unless valid?
 
-    body = []
+    array = []
     client = create_db_client
-    query = client.query("INSERT INTO hashtag (name) VALUES ('#{@name[0]}')")
-    query_lastid = client.query('SET @id = LAST_INSERT_ID();')
-    query_response = client.query('SELECT hashtag_id FROM hashtag WHERE hashtag_id = @id')
+    insert = client.query("INSERT INTO hashtag (name) VALUES ('#{@name[0]}')")
+    last_insert_id = client.query('SET @id = LAST_INSERT_ID();')
+    response = client.query('SELECT hashtag_id FROM hashtag WHERE hashtag_id = @id')
 
-    query_response.each do |response|
-      return response['hashtag_id']
+    response.each do |res|
+      return res['hashtag_id']
     end
   end
 
-  def save_hashtag(post_id)
+  def post_hashtag(post_id)
     client = create_db_client
 
-    query = client.query("INSERT INTO post_tags (hashtag_id,post_id) VALUES (#{create_hashtag},#{post_id})")
+    insert = client.query("INSERT INTO post_tags (hashtag_id,post_id) VALUES (#{post},#{post_id})")
   end
-
-
 end
