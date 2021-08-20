b05a196cee76ce54fcade08e60eb1013018c4423
mihaamiharu <48645558+mihaamiharu@users.noreply.github.com>
2021-08-20 10:33:26 +0700

    create find hashtag function
    will return hashtag by name

--- models/hashtag.rb
+++ models/hashtag.rb
@@ -1,4 +1,8 @@
+require_relative '../db/db_connection'
+
 class Hashtag 
+
+  attr_reader :id, :name
   def initialize(params)
     @hashtag_id = params[:hashtag_id],
     @name = params[:name]
@@ -7,6 +11,13 @@ class Hashtag
   def valid?
     return false if @name.nil?
     true
+  end
+
+  def find_hashtag
+    client = create_db_client
+    find_hashtag = client.query("SELECT hashtag_id FROM hashtag WHERE hashtag.`name` LIKE '%#{name}%")
+
+    body = find_hashtag
   end
 
 end
--- spec/models/hashtag_spec.rb
+++ spec/models/hashtag_spec.rb
@@ -22,4 +22,25 @@ describe Hashtag do
       end
     end
   end
+
+  describe '#find_hashtag' do
+    it 'should return choosen hashtag data' do
+      hashtag_data = Hashtag.new(
+        hashtag_id: 1,
+        name: 'semangat',
+        created_at: '2021-08-21 10:19:23'
+      )
+      expected_result = {
+        'hashtag_id' => 1,
+        'name' => 'semangat',
+        'created_at' => '2021-08-21 10:19:23'
+      }
+      query = "SELECT hashtag_id FROM hashtag WHERE hashtag.`name` LIKE '%#{hashtag_data.name}%'"
+      mock = double
+      allow(Mysql2::Client).to receive(:new).and_return(mock)
+      expect(mock).to receive(:query).with(mock).and_return(expected_result['hashtag_id'])
+
+      hashtag_data.find_hashtag
+    end
+  end
 end
