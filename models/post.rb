0095108951c90ff89e19921b5c7754fecec4dd52
mihaamiharu <48645558+mihaamiharu@users.noreply.github.com>
2021-08-20 07:36:56 +0700

    add validation for empty caption
    add attr:reader in post class

--- models/post.rb
+++ models/post.rb
@@ -1,9 +1,13 @@
 class Post
+  attr_reader :username, :caption, :timestamp
   def initialize(params)
     @username = params[:username],
     @caption = params[:caption],
     @timestamp = params[:timestamp] || Time.now
   end
 
-  
+  def valid?
+    return false if @caption.nil?
+    true
+  end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -12,4 +12,28 @@ describe Post do
       expect(post_data).not_to be_nil
     end
   end
+
+  describe '#valid?' do
+    context 'assigned with valid params' do
+      it 'should return true' do
+        current_time = Time.now
+        post_data = Post.new(
+          username: 'mihaamiharu',
+          caption: 'Main game mulu',
+          timestamp: current_time
+        )
+        expect(post_data.valid?).to eq(true)
+      end
+    end
+
+    context 'when caption is empty' do
+      it 'should return false' do
+        post_data = Post.new(
+          username: 'mihaamiharu',
+          caption: nil,
+        )
+        expect(post_data.valid?).to eq(false)
+      end
+    end
+  end
 end
