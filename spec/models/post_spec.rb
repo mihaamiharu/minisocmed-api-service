472e1b5d00411df848521ebaa8fdc98fca8c207d
mihaamiharu <48645558+mihaamiharu@users.noreply.github.com>
2021-08-20 16:32:20 +0700

    test function to check hashtag on post or comment
    change @post_data caption value and @expected_result caption value to have hashtag
    Before 'Main game mulu' to 'Main #game mulu'

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -5,7 +5,7 @@ describe Post do
     @post_data = Post.new(
       post_id: 1,
       username: 'mihaamiharu',
-      caption: 'Main game mulu',
+      caption: 'Main #game mulu',
       timestamp: '2021-08-17 07:00:13',
       attachment: nil
     )
@@ -13,7 +13,7 @@ describe Post do
     @expected_result = {
         'post_id' => 1,
         'username' => 'mihaamiharu',
-        'caption' => 'Main game mulu',
+        'caption' => 'Main #game mulu',
         'timestamp' => '2021-08-17 07:00:13',
         'attachment' => nil
     }
@@ -74,5 +74,26 @@ describe Post do
     end
   end
 
-  
+  describe '#create_comment' do
+    context 'given valid params' do
+      it 'should write comment on a post' do
+        query = "INSERT INTO post (username, caption, attachment) VALUES 
+        ('#{@post_data.username}', '#{@post_data.caption}','#{@post_data.attachment}')"
+        mock = double
+        allow(mock).to receive(:last_id).and_return(1)
+        expect(mock).to receive(:query).with(mock).and_return(200)
+        @post_data.create_comment
+      end
+    end
+  end
+
+  describe '#check_hashtag' do
+    context 'when created post or comment have hashtag' do
+      it 'should return contained hashtag' do
+        result = %w[game]
+
+        expect(@post_data.check_hashtag).to eq(result)
+      end
+    end
+  end
 end
