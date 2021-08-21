e4b0e3a4e4e5ef75ebef0fc199f005ed3a6a4073
mihaamiharu <48645558+mihaamiharu@users.noreply.github.com>
2021-08-21 14:24:47 +0700

    create post_tags_controller

---
+++ controllers/post_tags_controller.rb
@@ -0,0 +1,4 @@
+require_relative '../models/post_tags'
+
+class PostTagsController
+end
---
+++ spec/controllers/post_tags_controller_spec.rb
@@ -0,0 +1,5 @@
+require_relative '../../controllers/post_tags_controller'
+
+describe PostTagsController do
+  
+end
