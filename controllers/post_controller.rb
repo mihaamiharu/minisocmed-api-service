a4d5f7c8743f42dd0f06ba1cee07d9e91cdafbdc
mihaamiharu <48645558+mihaamiharu@users.noreply.github.com>
2021-08-21 14:02:49 +0700

    add post_controller
    add function to create post
    create post will run if response code is 201
    when params has attachment, it will write it on public files

---
+++ controllers/post_controller.rb
@@ -0,0 +1,29 @@
+require_relative '../models/post'
+
+class PostController
+  def create_post(params)
+    array = []
+    post = Post.new(params)
+
+    if post.posts == 201
+      if params.key?('attachment') && params['attachment'].key?('filename')
+        file_name = params['attachment']['filename']
+        file = params['attachment']['tempfile']
+        file_path = "./public/files/#{file_name}"
+        attachment = file_name
+        File.open(file_path, 'wb') do |f|
+          f.write(file.read)
+        end
+      end
+      post.find_post.each do |res|
+        array.push(res)
+      end
+      {
+        'message' => 'Success',
+        'status' => 201,
+        'method' => 'POST',
+        'data' => array
+      }
+    end
+  end
+end
---
+++ spec/controllers/post_controller_spec.rb
@@ -0,0 +1,55 @@
+require_relative '../../controllers/post_controller'
+
+describe PostController do
+  before :each do
+    @controller = PostController.new
+  end
+  describe 'create_post' do
+    context 'when assign by valid params' do
+      it' should return a 201' do
+        expected_response = {
+          'message' => 'Success',
+          'status' => 201,
+          'method' => 'POST',
+          'data' => [{
+            'post_id' => 1,
+            'user_id' => 1,
+            'caption' => 'Main #game mulu',
+            'attachment' => 'image.jpeg',
+            'tag_id' => nil,
+            'created_at' => '2021-08-17 07:00:13'
+          }]
+        }
+
+        mock_attachment = double
+        post_data = {
+            'post_id' => 1,
+            'user_id' => 1,
+            'caption' => 'Main #game mulu',
+            'attachment' => mock_attachment,
+            'tag_id' => nil,
+            'created_at' => '2021-08-17 07:00:13'
+        }
+
+        
+        mock = double
+        allow(Post).to receive(:new).with(post_data).and_return(mock)
+        allow(mock).to receive(:find_post).and_return(expected_response['data'])
+        allow(mock).to receive(:posts).and_return(201)
+
+        allow(mock_attachment).to receive('[]').with('filename').and_return('image.jpeg')
+        allow(mock_attachment).to receive(:key?).with('filename').and_return(true)
+
+        mock_file = double
+        expect(mock_file).to receive(:write)
+        expect(mock_file).to receive(:read)
+        allow(mock_attachment).to receive('[]').with('tempfile').and_return(mock_file)
+
+        allow(@File).to receive(:open) { |&block| block.call(file) }
+        allow(@posts).to receive(:to_hash).and_return({})
+
+        result = @controller.create_post(post_data)
+      end
+    end
+  end
+end
