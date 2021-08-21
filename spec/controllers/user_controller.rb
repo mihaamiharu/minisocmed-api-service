91e5b791afb4c52a5a8a5084db5ceef519fe9ef7
mihaamiharu <48645558+mihaamiharu@users.noreply.github.com>
2021-08-21 10:50:31 +0700

    Revert "add test and user controller"
    This reverts commit ff983c942fc573fa15cf8b7af5ef28ec0b64fcd5.

--- spec/controllers/user_controller.rb
+++ spec/controllers/user_controller.rb
@@ -8,26 +8,22 @@ describe UserController do
           'message' => 'Success',
           'status' => 200,
           'method' => 'POST',
-          'data' => [{
-            'user_id' => 1,
+          'body' => [{
             'username' => 'mihaamiharu',
             'email' => 'mihaamiharu.com',
-            'bio' => 'au ah gelap',
-            'created_at' => '2021-08-21 01:17:03',
+            'bio' => 'au ah gelap'
           }]
         }
 
         mock =double
         allow(User).to receive(:new).and_return(mock)
-        expect(mock).to receive(:find_user).and_return(expected_response['data'])
+        expect(mock).to receive(:find_user).and_return(expected_response['body'])
         expect(mock).to receive(:create_user).and_return(200)
 
         hash_user = {
-          'user_id' => 1,
           'username' => 'mihaamiharu',
           'email' => 'mihaamiharu.com',
-          'bio' => 'au ah gelap',
-          'created_at' => '2021-08-21 01:17:03',
+          'bio' => 'au ah gelap'
         }
 
         controller = UserController.new
@@ -40,7 +36,7 @@ describe UserController do
       it 'returns a 400' do
         mock = double
         allow(User).to receive(:new).and_return(mock)
-        
+        expect(mock).to receive(:find_user).and_return([])
         expect(mock).to receive(:create_user)
 
         expected_response = {
