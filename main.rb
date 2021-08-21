5de9e4aa120a8f614ce818e7c7a57e66d6a5f9e1
mihaamiharu <48645558+mihaamiharu@users.noreply.github.com>
2021-08-21 16:02:10 +0700

    add routing use gem json to convert response

--- main.rb
+++ main.rb
@@ -2,15 +2,42 @@ require 'sinatra'
 require 'json'
 
 require_relative './controllers/user_controller'
+require_relative './controllers/post_controller'
+require_relative './controllers/post_tags_controller'
 
 before do
   content_type :json
 end
 
+get '/api/' do
+  response = 'Hello!'
+  return response.to_json
+end
 
-post '/create_user' do
+post '/api/user/register' do
   controller = UsersController.new
   response = controller.create_new_user(params)
+  status response['status']
+  return response.to_json
+end
+
+post '/api/hashtag/details' do
+  controller = PostTagsController.new
+  response = controller.find_post_tags(params)
+  status response['status']
+  return response.to_json
+end
+
+get '/api/hashtag/trending' do
+  controller = PostTagsController.new
+  response = controller.find_trending_hashtag
+  status response['status']
+  return response.to_json
+end
+
+post '/api/post/create' do
+  controller = PostController.new
+  response = controller.create_post(params)
   status response['status']
   return response.to_json
 end
